38dfd1dbaa9f5119548a3157d4fa2298d00c41ee
James Simmonds <james.e.simmonds@gmail.com>
2021-03-05 20:44:25 +0000

    Logic for testing if edge shared with triangles

--- edge-compare.js
+++ edge-compare.js
@@ -11,3 +11,28 @@ function edgesAreEqual(edge1, edge2) {
     }
 }
 
+// We only want to add edges that aren't shared by other triangles in the set
+function edgeNotShared(edge, thisTriangle, triangles) {
+    let result = true;
+
+    for (let i = 0; i < triangles.length; i++) {
+        let triangle = triangles[i];
+
+        if (triangle != thisTriangle) {
+
+            let triEdge1 = triangle.e1;
+            let triEdge2 = triangle.e2;
+            let triEdge3 = triangle.e3;
+
+            let sharedWithE1 = edgesAreEqual(edge, triEdge1);
+            let sharedWithE2 = edgesAreEqual(edge, triEdge2);
+            let sharedWithE3 = edgesAreEqual(edge, triEdge3);
+
+            if (sharedWithE1 || sharedWithE2 || sharedWithE3) {
+                result = false;
+            }
+        }
+    }
+
+    return result;
+}
--- tests.js
+++ tests.js
@@ -1,4 +1,4 @@
-// window.addEventListener("load", test_edgeCompare);
+// window.addEventListener("load", test_edgeNotShared);
 
 function test_edgeCompare () {
 
@@ -17,5 +17,24 @@ function test_edgeCompare () {
     console.log(edgesAreEqual(e2, e2)); // True
     console.log(edgesAreEqual(e3, e2)); // False
     console.log(edgesAreEqual(e1, e3)); // False
+
+}
+
+function test_edgeNotShared () {
+
+    let v1 = new Vertex("v1", 50, 50);
+    let v2 = new Vertex("v2", 100, 100);
+    let v3 = new Vertex("v3", 150, 50);
+    let v4 = new Vertex("v4", 200, 150);
+
+    let t1 = new Triangle(v1, v2, v3);
+    let t2 = new Triangle(v2, v3, v4);
+
+    console.log(edgeNotShared(t1.e1, t1, [t1,t2]));
+    console.log(edgeNotShared(t1.e2, t1, [t1,t2]));
+    console.log(edgeNotShared(t1.e3, t1, [t1,t2]));
+    console.log(edgeNotShared(t2.e1, t2, [t1,t2]));
+    console.log(edgeNotShared(t2.e2, t2, [t1,t2]));
+    console.log(edgeNotShared(t2.e3, t2, [t1,t2]));
 
 }
